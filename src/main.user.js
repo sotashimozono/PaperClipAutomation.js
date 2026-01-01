89b05da76c13c3216c87aacb653949c2a46eb8e7
sotashimozono <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2026-01-01 09:21:52 +0900

    initial commit, configurations with javascript are almost done

---
+++ src/main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         PaperClip Solver Automation
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automate Universal Paperclips
+// @author       Your Name
+// @match        https://www.decisionproblem.com/paperclips/
+// @grant        none
+// @require      https://raw.githubusercontent.com/sotashimozono/PaperClipAutomation.js/main/src/solver.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Automation Loader Started");
+    // ここに本体（solver.js）を読み込んだ後の初期化処理を書く
+})();
